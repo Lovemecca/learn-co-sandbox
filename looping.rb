c559795c332fed130c61049a782acc7158dac20b
Mecca Jay <mecca.nicolej@gmail.com>
2020-03-13 22:01:33 +0000

    Automatically backed up by Learn

---
+++ looping.rb
@@ -0,0 +1,15 @@
+loop do
+   puts "Never pass a school bus if it has flashing red lights"
+end
+
+looping
+  
+  
+  
+  brothers = ["Tim", "Tom", "Jim"]
+counter = 1
+brothers.each do |brother|
+  puts "This is loop number #{counter}"
+  puts "Stop hitting yourself #{brother}!"
+  counter += 1
+end
